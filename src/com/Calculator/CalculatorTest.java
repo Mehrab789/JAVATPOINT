eacf1e855bc383e390a4d07bdc0e256db56244c3
Mehrab789 <70692540+Mehrab789@users.noreply.github.com>
2020-09-29 15:43:48 +0600

    New Update 9/29/2020

--- src/com/Calculator/CalculatorTest.java
+++ src/com/Calculator/CalculatorTest.java
@@ -2,6 +2,16 @@ package com.Calculator;
 
 public class CalculatorTest {
 
+    int instanceVariable;
+
+     CalculatorTest(){
+        instanceVariable = 202;
+    }
+
+    CalculatorTest(int value1, int value2){
+        instanceVariable=value1+value2;
+    }
+
     public Integer add (Integer arg1, Integer arg2) {
         arg1=1000;
         Integer result = arg1+arg2;
@@ -14,10 +24,17 @@ public class CalculatorTest {
     }
 
     public static void main(String[] args) {
-        CalculatorTest calc = new CalculatorTest();
+        CalculatorTest calc = new CalculatorTest(110,21);
         int arg1 = 56;
         int arg2 = 36;
         int arg3 = 76;
+
+        System.out.println("Printing the value of instance variable - normal calculator " + calc.instanceVariable);
+
+        /* CalculatorTest normalCalculator = new CalculatorTest(23,40);
+        CalculatorTest scientificCalculator = new CalculatorTest(27,50);
+        System.out.println("Printing the value of instance variable - normal calculator " + normalCalculator.instanceVariable);
+        System.out.println("Printing the value of instance variable - scientific calculator " + scientificCalculator.instanceVariable);
         System.out.println("Printing arg1 before passing it by value "+arg1);
         Integer result = calc.add(arg1,arg2);
         System.out.println("Printing arg1 after passing it by value to add method  "+arg1);
@@ -25,5 +42,6 @@ public class CalculatorTest {
 
         Integer resultFromNewMethod = calc.add(arg1,arg2,arg3);
         System.out.println("Result from new Add method "+resultFromNewMethod);
+    */
     }
 }
---
+++ src/com/Collection/Collection.java
@@ -0,0 +1,17 @@
+package com.Collection;
+
+import java.util.*;
+class Collection{
+    public static void main(String args[]){
+        ArrayList<String> list=new ArrayList<String>();//Creating arraylist
+        list.add("Ravi");//Adding object in arraylist
+        list.add("Vijay");
+        list.add("Ravi");
+        list.add("Ajay");
+        //Traversing list through Iterator
+        Iterator itr=list.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
